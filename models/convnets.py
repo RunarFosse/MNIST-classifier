6f5e004aef2dc43395249450cd7d2afbeb321a29
Runar Fosse <rfosse01@gmail.com>
2024-01-10 13:19:35 +0100

    Changed model architectures to have around equal n.o. parameters (109k)

--- models/convnets.py
+++ models/convnets.py
@@ -14,9 +14,9 @@ class ConvNet1(nn.Module):
         self.pool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=2) # 3x3
 
         # Fully connected layers
-        self.fc1 = nn.Linear(64 * (3 * 3), 256)
-        self.fc2 = nn.Linear(256, 64)
-        self.fc3 = nn.Linear(64, 10)
+        self.fc1 = nn.Linear(64 * (3 * 3), 144)
+        self.fc2 = nn.Linear(144, 96)
+        self.fc3 = nn.Linear(96, 10)
 
     def forward(self, x):
         # Convolutional part
@@ -42,12 +42,12 @@ class ConvNet2(nn.Module):
         self.conv1 = nn.Conv2d(1, 8, kernel_size=(3, 3), padding=1) # 28x28
         self.pool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=2) # 14x14
         self.conv2 = nn.Conv2d(8, 32, kernel_size=(3, 3)) # 12x12
-        self.conv3 = nn.Conv2d(32, 128, kernel_size=(5, 5)) # 8x8
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=(5, 5)) # 8x8
         self.pool2 = nn.MaxPool2d(kernel_size=(3, 3), stride=2) # 3x3
 
         # Fully connected layers
-        self.fc1 = nn.Linear(128 * (3 * 3), 256)
-        self.fc2 = nn.Linear(256, 64)
+        self.fc1 = nn.Linear(64 * (3 * 3), 86)
+        self.fc2 = nn.Linear(86, 64)
         self.fc3 = nn.Linear(64, 10)
     
     def forward(self, x):
@@ -76,8 +76,8 @@ class ConvNet3(nn.Module):
         self.pool2 = nn.MaxPool2d(kernel_size=(3, 3), stride=2) # 5x5
 
         # Fully connected layers
-        self.fc1 = nn.Linear(32 * (5 * 5), 64)
-        self.fc2 = nn.Linear(64, 10)
+        self.fc1 = nn.Linear(32 * (5 * 5), 128)
+        self.fc2 = nn.Linear(128, 10)
     
     def forward(self, x):
         # Convolutional part
